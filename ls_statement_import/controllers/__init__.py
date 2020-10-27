2c0c86d0b9d997e3278c99f5fa20cea2069bfa1f
Marcelo Mora <marcelo.mora@accioma.com>
2020-10-27 02:19:10 +0000

    [12.0][ADD] Statement Import

---
+++ ls_statement_import/controllers/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Linksoft Mitra Informatika
+
+from . import controllers
+
+__all__ = ['controllers']
---
+++ ls_statement_import/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Linksoft Mitra Informatika
+
+from . import models
+
+__all__ = ['models']
