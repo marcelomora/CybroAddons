871b4e14beb8cbf5934ad75776798736112246a9
marcelomora <java.diablo@gmail.com>
2021-07-14 13:34:09 -0500

    [12.0][FIX] VAT 0 not considered, it will be created by pos_darwin

--- pos_darwin/models/account_invoice.py
+++ pos_darwin/models/account_invoice.py
@@ -4,23 +4,42 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import _, api, fields, models
+from odoo.exceptions import Warning
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    @api.multi
-    def invoice_validate(self):
-        """Override method for sequence assigning"""
-        taxes = self.env['account.tax'].search([('code_base', '=', '413')])
-        tax = [t.id for t in taxes][0]
-        for inv in self:
-            if inv.type not in ('out_invoice', 'out_refund'):
+    @api.model
+    def validate_draft(self):
+        invs = self.search([
+            ('state', '=', 'draft'),
+            ('type', '=', 'out_invoice'),
+            ], limit = 200, order = 'id desc')
+
+        for i in invs:
+            # _logger.info("Factura {}".format(i.id))
+            try:
+                i.action_invoice_open()
+            except:
                 continue
-            for line in inv.invoice_line_ids:
-                if not line.invoice_line_tax_ids:
-                    tx = [(6, 0, (tax,))]
-                    line.write({'invoice_line_tax_ids': tx})
 
-            inv._compute_amount()
-        return super(AccountInvoice, self).invoice_validate()
+    @api.model
+    def compute_amount(self):
 
+        invs = self.search([
+            ('type', '=', 'out_invoice'),
+            ('state', 'in', ('open', 'paid', ))
+            ], order="id asc")
+
+        for i in invs:
+            i._onchange_invoice_line_ids()
+            i._compute_amount()
+            _logger.info("FActura base vat 0 {} {}".format(i.id, i.amount_base_vat_0))
+
+    @api.multi
+    def compute_amount_by_id(self):
+        self.ensure_one()
+
+        self._onchange_invoice_line_ids()
+        self._compute_amount()
+        return "Factura base vat 0 {} {}".format(self.id, self.amount_base_vat_0)
